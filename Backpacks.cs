904d60b8d1e19d122ae4b0baf5e2e5ad09937487
WhiteThunder <whetethunger@gmail.com>
2023-01-16 11:46:38 -0800

    Increment minor version

--- Backpacks.cs
+++ Backpacks.cs
@@ -25,7 +25,7 @@ using UnityEngine.UI;
 
 namespace Oxide.Plugins
 {
-    [Info("Backpacks", "LaserHydra", "3.8.0")]
+    [Info("Backpacks", "LaserHydra", "3.9.0")]
     [Description("Allows players to have a Backpack which provides them extra inventory space.")]
     internal class Backpacks : CovalencePlugin
     {
